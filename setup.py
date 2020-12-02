2cbd064fc033a38be03e2fe709f23e7b049f211d
Anna Peng <anna.peng86@gmail.com>
2020-12-02 21:29:44 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project takes user inputted keywords to map out experts within the specified fields',
+    author='Ella Franks and Anna Peng',
+    license='',
+)
